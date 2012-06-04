330e32a5964178451a267eeab479e299f7ea1e62
Alfredo Deza <alfredodeza@gmail.com>
2012-06-03 20:00:29 -0400

    temporary fix for tree_path building

--- notario/engine.py
+++ notario/engine.py
@@ -6,20 +6,20 @@ class Validator(object):
     
     def __init__(self, data, schema, **kw):
         self.data = normalize(data)
-        self.schema = normalize(schema, sort=False)
+        self.schema = normalize_schema(schema)
         self.key_tree = ""
         self.value_tree = ""
 
     def validate(self):
         for i in range(len(self.data)):
             key, value = self.data.get(i)
-            self.key_tree + " %s" % key
+            key_tree = "%s %s" % (self.key_tree, key)
             skey, svalue = self.schema.get(i, (None, None))
-            self.value_tree + " %s" % value
+            value_tree = "%s %s" % (self.value_tree, value)
             if (skey, svalue) == (None, None):
                 continue
-            enforce(key, skey, self.key_tree)
-            enforce(value, svalue, self.value_tree)
+            enforce(key, skey, key_tree)
+            enforce(value, svalue, value_tree)
 
 
 def normalize(data_structure, sort=True):
@@ -28,8 +28,29 @@ def normalize(data_structure, sort=True):
     dictionary, with integers as keys for tuples.
     """
     if sort:
+        for k, v in data_structure.items():
+            if isinstance(v, dict):
+                data_structure[k] = normalize(v)
         data_structure = sorted(data_structure.items())
     return dict((number, value) for number, value in enumerate(data_structure))
+
+
+def normalize_schema(data_structure):
+    normalized = {}
+    for number, value in enumerate(data_structure):
+        if isinstance(value[1], tuple): # a nested tuple
+            normalized[number] = normalize_schema(value[1])
+        else:
+            normalized[number] = value
+    return normalized
+    #for i in range(len(mapped)):
+        #k, v = mapped.get(i)
+    #for k, v in mapped.items():
+        #if isinstance(v, tuple):
+            #mapped[i][k] = dict((number, value) for number, value in enumerate(v))
+
+    #return mapped
+
 
 
 def enforce(data_item, schema_item, tree):
