df03f7a3de9c214b5b771cbb3fe65467c6c595df
Alfredo Deza <alfredodeza@gmail.com>
2012-06-06 08:48:03 -0400

    new exception for SchemaErrors

--- notario/exceptions.py
+++ notario/exceptions.py
@@ -18,12 +18,14 @@ class Invalid(Exception):
     def __str__(self):
         return self._get_message()
 
-    def _format_path(self):
+    def _format_path(self, use_pair=True):
         message = ""
         for key in self.path:
             accessed_key = '-> %s ' % key
             message += accessed_key
-        return message + self._pair
+        if use_pair:
+            return message + self._pair
+        return message
 
     def _get_message(self):
         if is_callable(self.schema_item):
@@ -40,3 +42,10 @@ class Invalid(Exception):
             return self._reason[0]
         except IndexError:
             return self._reason
+
+
+class SchemaError(Invalid):
+
+    def _get_message(self):
+        msg = "%s %s" % (self._format_path(use_pair=False), self._reason)
+        return msg
