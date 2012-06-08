4f7ede94de42cf2b797eb3f85947b6f174432981
Alfredo Deza <alfredodeza@gmail.com>
2012-06-08 15:59:20 -0400

    making function signatures more pep8

--- notario/validators/types.py
+++ notario/validators/types.py
@@ -1,33 +1,35 @@
+"""
+Basic type validators
+"""
 
-
-def String(value):
+def string(value):
     """
     Validates a given input is of type string.
     """
     assert isinstance(value, basestring), "not of type string"
 
 
-def Boolean(value):
+def boolean(value):
     """
     Validates a given input is of type boolean.
     """
     assert isinstance(value, bool), "not of type boolean"
 
 
-def Dictionary(value):
+def dictionary(value):
     """
     Validates a given input is of type dictionary.
     """
     assert isinstance(value, dict), "not of type dictionary"
 
 
-def Array(value):
+def array(value):
     """
     Validates a given input is of type list.
     """
     assert isinstance(value, list), "not of type array"
 
-def Integer(value):
+def integer(value):
     """
     Validates a given input is of type int..
     """
