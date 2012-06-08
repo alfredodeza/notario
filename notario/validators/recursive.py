acc7296cbebaca9aed47e57f04f377f662b4472f
Alfredo Deza <alfredodeza@gmail.com>
2012-06-07 21:28:33 -0400

    create the recursive validators

---
+++ notario/validators/recursive.py
@@ -0,0 +1,35 @@
+from notario.engine import Validator
+
+
+class BasicRecursiveValidator(object):
+    """
+    Base class for recursive validators, can be sub-classed
+    for other type of recursive validators but should not be
+    used directly.
+    """
+
+    __validator_iterable__ = True
+
+    def __init__(self, schema):
+        self.schema = schema
+
+
+class AnyObject(BasicRecursiveValidator):
+    """
+    Grab the first object in a nested dictionary and apply the schema needed
+    """
+
+    def __call__(self, data, tree):
+        index = len(data) - 1
+        validator = Validator(data, self.schema, tree, index=index)
+        validator.validate()
+
+
+class AllObjects(BasicRecursiveValidator):
+    """
+    For all the objects contained in a dictionary apply the schema passed in.
+    """
+
+    def __call__(self, data, tree):
+        validator = Validator(data, self.schema, tree)
+        validator.validate()
