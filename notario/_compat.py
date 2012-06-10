e4c5e9b8bfcd0e63a4c64aff81a2bcd86cfe0d58
Alfredo Deza <alfredodeza@gmail.com>
2012-06-10 14:14:08 -0400

    create a new compatibility module for py3k

---
+++ notario/_compat.py
@@ -0,0 +1,10 @@
+"""
+A basic module to provide both Python2 and Python3 support
+on the same source
+"""
+import sys
+
+if sys.version_info >= (3, 0):
+    basestring = str
+else:
+    basestring = basestring
--- notario/validators/types.py
+++ notario/validators/types.py
@@ -1,6 +1,7 @@
 """
 Basic type validators
 """
+from notario._compat import basestring
 
 def string(value):
     """
