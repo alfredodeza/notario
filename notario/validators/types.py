1e0caa70fd34eb74bb46a7c73838438aeae20e3f
Alfredo Deza <alfredodeza@gmail.com>
2012-06-18 09:13:39 -0400

    forgot to add the docstrings for type validators

--- notario/validators/types.py
+++ notario/validators/types.py
@@ -6,6 +6,11 @@ from notario._compat import basestring
 def string(value):
     """
     Validates a given input is of type string.
+
+    Example usage::
+
+        data = {'a' : 21}
+        schema = (string, 21)
     """
     assert isinstance(value, basestring), "not of type string"
 
@@ -13,6 +18,11 @@ def string(value):
 def boolean(value):
     """
     Validates a given input is of type boolean.
+
+    Example usage::
+
+        data = {'a' : True}
+        schema = ('a', boolean)
     """
     assert isinstance(value, bool), "not of type boolean"
 
@@ -20,6 +30,11 @@ def boolean(value):
 def dictionary(value):
     """
     Validates a given input is of type dictionary.
+
+    Example usage::
+
+        data = {'a' : {'b': 1}}
+        schema = ('a', dictionary)
     """
     assert isinstance(value, dict), "not of type dictionary"
 
@@ -27,11 +42,21 @@ def dictionary(value):
 def array(value):
     """
     Validates a given input is of type list.
+
+    Example usage::
+
+        data = {'a' : [1,2]}
+        schema = ('a', array)
     """
     assert isinstance(value, list), "not of type array"
 
 def integer(value):
     """
     Validates a given input is of type int..
+
+    Example usage::
+
+        data = {'a' : 21}
+        schema = ('a', integer)
     """
     assert isinstance(value, int), "not of type int"
