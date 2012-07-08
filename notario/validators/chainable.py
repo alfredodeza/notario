7a5cca8a495d8e6b4e14f3357a58bccadf50f45b
Alfredo Deza <alfredodeza@gmail.com>
2012-07-08 13:02:25 -0400

    bare bone classes for the chainable validators

--- notario/validators/chainable.py
+++ notario/validators/chainable.py
@@ -3,3 +3,17 @@ Chainable validators are *encapsulating* validators. They
 usually will not validate per se, but can contain other validators
 inside them and pass the value to them.
 """
+
+
+class AllIn(object):
+    """
+    Validates against all the validators passed in.
+    """
+    pass
+
+
+class AnyIn(object):
+    """
+    If any contained validator passes it skips any others
+    """
+    pass
