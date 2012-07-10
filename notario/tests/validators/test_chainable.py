8496a257cd71cdb6939312354caece2acbf48718
Alfredo Deza <alfredodeza@gmail.com>
2012-07-10 09:23:49 -0400

    test for validity in callable validators

--- notario/tests/validators/test_chainable.py
+++ notario/tests/validators/test_chainable.py
@@ -9,3 +9,9 @@ class TestBasicChainValidator(object):
         with raises(TypeError) as exc:
             chainable.BasicChainValidator("not a callable")
         assert exc.value.args[0] == "got a non-callable argument: 'not a callable'"
+
+    def test_assigns_validators_if_all_are_callable(self):
+        def foo(): pass
+        def bar(): pass
+        chain = chainable.BasicChainValidator(foo, bar)
+        assert chain.args == (foo, bar)
