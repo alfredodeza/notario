41a90b38cbb5b0a8dd6b93a6f00cdb0a6e608c6f
Alfredo Deza <alfredodeza@gmail.com>
2013-02-06 11:52:28 -0500

    add tests for the hybrid validators

--- notario/tests/validators/test_types.py
+++ notario/tests/validators/test_types.py
@@ -49,3 +49,87 @@ class TestTypes(object):
         with raises(AssertionError) as exc:
             types.integer('a string')
         assert exc.value.args[0] == 'not of type int'
+        
+
+class TestTypesAsDecorators(object):
+
+    def test_string_pass(self):
+        @types.string
+        def validate(value):
+            assert value == "a string"
+
+        assert validate('a string') is None
+
+    def test_boolean_pass(self):
+        @types.boolean
+        def validate(value):
+            assert value is False
+
+        assert types.boolean(False) is None
+
+    def test_dictionary_pass(self):
+        @types.dictionary
+        def validate(value):
+            assert value == {}
+
+        assert types.dictionary({}) is None
+
+    def test_array_pass(self):
+        @types.array
+        def validate(value):
+            assert value == []
+
+        assert types.array([]) is None
+
+    def test_integer_pass(self):
+        @types.integer
+        def validate(value):
+            assert value == 1
+
+        assert types.integer(1) is None
+
+    def test_string_fail(self):
+        @types.string
+        def validate(value):
+            assert value == 'a string'
+
+        with raises(AssertionError) as exc:
+            validate(1)
+        assert exc.value.args[0] == 'not of type string'
+
+    def test_boolean_fail(self):
+        @types.boolean
+        def validate(value):
+            assert value is False
+        
+        with raises(AssertionError) as exc:
+            validate('a string')
+        assert exc.value.args[0] == 'not of type boolean'
+
+    def test_dictionary_fail(self):
+        @types.dictionary
+        def validate(value):
+            assert value == {}
+        
+        with raises(AssertionError) as exc:
+            validate([])
+        assert exc.value.args[0] == 'not of type dictionary'
+
+    def test_array_fail(self):
+        @types.array
+        def validate(value):
+            assert value is False
+        
+        with raises(AssertionError) as exc:
+            validate({})
+        assert exc.value.args[0] == 'not of type array'
+
+    def test_integer_fail(self):
+        @types.integer
+        def validate(value):
+            assert value == 1
+
+        with raises(AssertionError) as exc:
+            validate('a string')
+        assert exc.value.args[0] == 'not of type int'
+    
