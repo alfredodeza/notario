b0c5c8d7ba76ed0675a6ec21e799abf00dc62421
Alfredo Deza <alfredodeza@gmail.com>
2013-02-08 15:21:04 -0500

    granular tests for the optional decorator

--- notario/tests/test_utils.py
+++ notario/tests/test_utils.py
@@ -4,7 +4,8 @@ from notario import utils
 class TestIsCallable(object):
 
     def setup(self):
-        def fake_callable(): pass
+        def fake_callable():
+            pass
         self.fake_callable = fake_callable
 
     def test_is_callable(self):
@@ -28,3 +29,11 @@ class TestOptionalDecorator(object):
     def test_value(self):
         result = utils.optional(self.fake_validator)('some value!')
         assert result is True
+
+    def test_decorator_is_marked_as_optional(self):
+        result = utils.optional('')
+        assert result.is_optional is True
+
+    def test_value_has_string(self):
+        result = utils.optional('value')
+        assert result() == 'value'
