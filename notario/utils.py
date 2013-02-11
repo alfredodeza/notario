1f2d277712d274c9591d6ef2d32c154dad1ccca6
Alfredo Deza <alfredodeza@gmail.com>
2013-02-11 15:15:51 -0500

    create the sifting and sorting utilities

--- notario/utils.py
+++ notario/utils.py
@@ -23,3 +23,48 @@ def optional(validator):
     msg = "import optional from notario.decorators, not from utils"
     warnings.warn(msg, DeprecationWarning, stacklevel=2)
     return decorators.optional(validator)
+
+
+class ndict(dict):
+    """
+    Used by Notario so that it can slap attributes to the object
+    when it is created, something that regular dictionaries will not
+    allow to do.
+    """
+    pass
+
+
+def re_sort(data):
+    """
+    A data with keys that are not enumerated sequentially will be
+    re sorted and sequentially ordered.
+
+    For example::
+
+        data = {16: ('1', 'b'), 3: ('1', 'a')}
+
+    Will become::
+
+        >>> re_sort(data)
+        >>> {0: ('1', 'a'), 1: ('1', 'b')}
+        
+    """
+    keys = sorted(data.keys())
+    new_data = {}
+    for number, key in enumerate(keys):
+        new_data[number] = data[key]
+    return new_data
+
+
+def sift(data, required_items=None):
+    """
+    Receive a ``data`` object that will be in the form
+    of a normalized structure (e.g. ``{0: {'a': 0}}``) and
+    filter out keys that match the ``required_items``.
+    """
+    required_items = required_items or []
+    new_data = {}
+    for k, v in data.items():
+        if v[0] in required_items:
+            new_data[k] = v
+    return re_sort(new_data)
