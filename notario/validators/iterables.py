7754ad0a7057f56068c540739ff0304f753c83c3
Alfredo Deza <alfredodeza@gmail.com>
2013-02-21 13:33:04 -0500

    created a MultiIterable validator based on MultiRecursive

--- notario/validators/iterables.py
+++ notario/validators/iterables.py
@@ -4,7 +4,7 @@ applying a schema to any given items in an array.
 """
 from notario.exceptions import Invalid
 from notario.engine import IterableValidator
-from notario.utils import is_callable, safe_repr, expand_schema
+from notario.utils import is_callable, safe_repr, expand_schema, is_schema
 
 
 class BasicIterableValidator(object):
@@ -142,3 +142,89 @@ class AllItems(BasicIterableValidator):
         self.safe_type(data, tree)
         validator = IterableValidator(data, schema, tree, name='AllItems')
         validator.validate()
+
+
+class MultiIterable(object):
+
+    """
+    .. testsetup:: *
+
+        from notario import validate
+        from notario.validators.iterables import MultiIterable
+
+    This validator is useful when there is a need for validating any number of
+    schemas for a given data set.
+
+    If the requirement is, for example, to validate either ``string: string``
+    or ``string: int`` this will not be possible with any of the other
+    validators in Notario because they all assume a rule that must dominate
+    everything. Otherwise, it is required to specify the expectation.
+
+    In the case that the object to validate complies with the above
+    requirement, this validator can accept **any number** of schemas as
+    arguments, so if one schema fails, the next one will be tried until all the
+    schemas are applied for a given item. The ``MultiRecursive`` validator will
+    look like this in order to pass the incoming data:
+
+    .. doctest:: multiiterable
+
+        >>> data = {'main': {'foo': 'bar'}}
+        >>> schema = ('main', MultiIterable(('foo', 1), ('foo', 'bar')))
+        >>> validate(data, schema)
+
+    Because we can't be sure what the data may hold we are forced to define
+    different rules and apply them so that they can pass. If we repeat the
+    expectation but an invalid value comes along, the validator will naturally
+    fail:
+
+    .. doctest:: mutlirecursive_fail
+
+        >>> data = {'main': [{'foo': False}]}
+        >>> schema = ('main', MultiIterable(('foo', 1), ('foo', 'bar')))
+        >>> validate(data, schema)
+        Traceback (most recent call last):
+        ...
+        Invalid: -> foo -> False did not match 'bar'
+
+    """
+    __validator_leaf__ = True
+
+    def __init__(self, *schemas):
+        for schema in schemas:
+            if not is_schema(schema):
+                raise TypeError("got a non schema argument: %s" % safe_repr(schema))
+        self.schemas = schemas
+
+    def __call__(self, data, tree):
+        """
+        Go through each item in data against the first schema in the arguments.
+        If that fails try each subsequent schema until it passes. Even if
+        schemas are failing to apply to that given item, consume all the
+        available ones until at least one passes.
+
+        If nothing passes, the last error is raised.
+
+        :param data: Incoming data from the validator engine in normalized dict
+        form.
+        :param tree: The traversing tree up to this point, always passed in.
+        :raises Invalid: If none of the schemas can validate the data.
+        """
+        first_schema = expand_schema(self.schemas[0])
+        index = len(data) - 1
+        validator = IterableValidator(data, first_schema, [], index=index, name='MultiIterable')
+
+        for item_index in range(len(data)):
+            try:
+                validator.leaf(item_index)
+            except Invalid:
+                self.itemized_validation(validator, item_index)
+
+    def itemized_validation(self, validator, item_index):
+        for schema in self.schemas:
+            try:
+                validator.schema = expand_schema(schema)
+                validator.tree = []
+                return validator.leaf(item_index)
+            except Invalid, error:
+                error = error
+        raise error
